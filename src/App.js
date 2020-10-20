f9766eb603882ed751154ffa1b24e73755a3347e
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2020-10-20 19:46:28 +0300

    Create Project Structure and Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 class App extends Component {
     render() {
         return (
-            <div>
-                App
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
             </div>
         );
     }
